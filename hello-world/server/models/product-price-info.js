f688c206840c41df8ffe4368f4ce0dbde0becdee
Dexter Nguyen <lich.nguyen@amblique.com>
2018-01-18 20:54:13 +0700

    Added methods for product prices model

--- hello-world/server/models/product-price-info.js
+++ hello-world/server/models/product-price-info.js
@@ -1,5 +1,79 @@
 'use strict';
 
-module.exports = function(Productpriceinfo) {
+/**
+* Class: ProductPriceInfo
+* Description: Simple class representing a product price point. This class is useful because it provides additional information beyond just the price. Since the system calculates sales based on applicable price books, it is sometimes useful to know additional such as which price book defined a price book, what percentage discount off the base price this value represents, and the date range for which this price point is active.
+*/
+module.exports = function (ProductPriceInfo) {
 
+    /**
+    * @documentation: Returns the date from which the associated price point is valid. If such as date doesn't exist, e.g. as in the case of a continuous price point, null will be returned.
+    *
+    * @param  {Date} the date from which the associated price point is valid.
+    * @return {null}
+    */
+    ProductPriceInfo.prototype.getOnlineFrom = function () {
+        //TODO: Implement Me
+
+    };
+
+
+    /**
+    * @documentation: Returns the date until which the associated price point is valid. If such a date doesn't exist, e.g. as in the case of a continuous price point, null will be returned.
+    *
+    * @param  {Date} the date to which the associated price point is valid
+    * @return {null}
+    */
+    ProductPriceInfo.prototype.getOnlineTo = function () {
+        //TODO: Implement Me
+
+    };
+
+
+    /**
+    * @documentation: Returns the percentage off value of this price point related to the base price for the product's minimum order quantity.
+    *
+    * @param  {Number} the percentage off value of this price point.
+    * @return {null}
+    */
+    ProductPriceInfo.prototype.getPercentage = function () {
+        //TODO: Implement Me
+
+    };
+
+
+    /**
+    * @documentation: Returns the price monetary price for this price point.
+    *
+    * @param  {[object Object]} the price amount.
+    * @return {null}
+    */
+    ProductPriceInfo.prototype.getPrice = function () {
+        //TODO: Implement Me
+
+    };
+
+
+    /**
+    * @documentation: Returns the price book which defined this price point.
+    *
+    * @param  {[object Object]} the price book defining this price
+    * @return {null}
+    */
+    ProductPriceInfo.prototype.getPriceBook = function () {
+        //TODO: Implement Me
+
+    };
+
+
+    /**
+    * @documentation: Returns the price info associated with this price point. This is an arbitrary string which a merchant can associate with a price entry. This can be used for example, to track which back-end system the price is derived from.
+    *
+    * @param  {String} the price info associated with this price point.
+    * @return {null}
+    */
+    ProductPriceInfo.prototype.getPriceInfo = function () {
+        //TODO: Implement Me
+
+    };
 };
--- hello-world/server/models/product-price-table.js
+++ hello-world/server/models/product-price-table.js
@@ -1,5 +1,72 @@
 'use strict';
 
-module.exports = function(Productpricetable) {
+/**
+* Class: ProductPriceTable
+* Description: A ProductPriceTable is a map of quantities to prices representing the potentially tiered prices of a product in Commerce Cloud Digital. The price of a product is the price associated with the largest quantity in the ProductPriceTable which does not exceed the purchase quantity.
+*/
+module.exports = function (ProductPriceTable) {
+
+    /**
+    * @documentation: Returns the quantity following the passed quantity in the price table. If the passed quantity is the last entry in the price table, null is returned.
+    *
+    * @param quantity {[object Object]} the quantity to use to locate the next quantity in the price table.
+    * @param  {[object Object]} the next quantity or null.
+    * @return {null}
+    */
+    ProductPriceTable.prototype.getNextQuantity = function (quantity) {
+        //TODO: Implement Me
+
+    };
+
+
+    /**
+    * @documentation: Returns the percentage off value of the price related to the passed quantity, calculated based on the price of the products minimum order quantity.
+    *
+    * @param quantity {[object Object]} the price quantity to compute the percentage off.
+    * @param  {Number} the percentage off value of the price related to the passed quantity.
+    * @return {null}
+    */
+    ProductPriceTable.prototype.gerPercentage = function (quantity) {
+        //TODO: Implement Me
+
+    };
+
+
+    /**
+    * @documentation: Returns the monetary price for the passed order quantity. If no price is defined for the passed quantity, null is returned. This can happen if for example no price is defined for a single item.
+    *
+    * @param quantity {[object Object]} the quantity to use to determine price.
+    * @param  {[object Object]} price amount for the passed quantity.
+    * @return {null}
+    */
+    ProductPriceTable.prototype.getPrice = function (quantity) {
+        //TODO: Implement Me
+
+    };
+
+
+    /**
+    * @documentation: Returns the price book which defined the monetary price for the passed order quantity. If no price is defined for the passed quantity, null is returned. This can happen if for example no price is defined for a single item.
+    *
+    * @param quantity {[object Object]} the quantity to use to determine price.
+    * @param  {[object Object]} the price book defining this price, or null
+    * @return {null}
+    */
+    ProductPriceTable.prototype.getPriceBook = function (quantity) {
+        //TODO: Implement Me
+
+    };
+
+
+    /**
+    * @documentation: Returns all quantities stored in the price table.
+    *
+    * @param  {[object Object]} all price table quantities.
+    * @return {null}
+    */
+    ProductPriceTable.prototype.getQuantities = function () {
+        //TODO: Implement Me
+
+    };
 
 };
